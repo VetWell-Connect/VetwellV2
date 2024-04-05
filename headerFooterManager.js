c5e576769c4d299dce871825d233dcac5a7299b1
UcheOsujiBison <91709768+UcheOsujiBison@users.noreply.github.com>
2024-04-05 19:42:47 -0400

    changed bio on about page and fixed footer

--- headerFooterManager.js
+++ headerFooterManager.js
@@ -133,7 +133,7 @@ class SpecialFooter extends HTMLElement {
             </div>
 
             <div class="container copyright text-center mt-4">
-            <p>© <span>Copyright</span> <strong class="px-1">VetWell Conncet</strong> <span>All Rights Reserved</span></p>
+            <p>© <span>Copyright</span> <strong class="px-1">VetWell Connect</strong> <span>All Rights Reserved</span></p>
             <div class="credits">
                 <!-- All the links in the footer should remain intact. -->
                 <!-- You can delete the links only if you've purchased the pro version. -->
